57ef3ab8502dfabadaa88f677bc857245c962620
Sangjun_Park <sangjun.park.jun@gmail.com>
2020-10-04 14:27:10 +0900

    Add files via upload

--- Dr_Park Cheat Sheet.cpp
+++ Dr_Park Cheat Sheet.cpp
@@ -1674,3 +1674,344 @@ vector<P> triangulate(vector<P> pts) {
 	while (qi < sz(q)) if (!(e = q[qi++])->mark) ADD;
 	return pts;
 }
+
+==============
+//SMAWK Algorithm
+typedef unsigned long ulong;
+
+/*
+ *  Internal implementation of the SMAWK algorithm.
+ */
+template <typename T>
+void _smawk(
+        const vector<ulong>& rows,
+        const vector<ulong>& cols,
+        const function<T(ulong, ulong)>& lookup,
+        vector<ulong>* result) {
+    // Recursion base case
+    if (rows.size() == 0) return;
+
+    // ********************************
+    // * REDUCE
+    // ********************************
+
+    vector<ulong> _cols;  // Stack of surviving columns
+    for (ulong col : cols) {
+        while (true) {
+            if (_cols.size() == 0) break;
+            ulong row = rows[_cols.size() – 1];
+            if (lookup(row, col) >= lookup(row, _cols.back()))
+                break;
+            _cols.pop_back();
+        }
+        if (_cols.size() < rows.size())
+            _cols.push_back(col);
+    }
+
+    // Call recursively on odd-indexed rows
+    vector<ulong> odd_rows;
+    for (ulong i = 1; i < rows.size(); i += 2) {
+        odd_rows.push_back(rows[i]);
+    }
+    _smawk(odd_rows, _cols, lookup, result);
+
+    unordered_map<ulong, ulong> col_idx_lookup;
+    for (ulong idx = 0; idx < _cols.size(); ++idx) {
+        col_idx_lookup[_cols[idx]] = idx;
+    }
+
+    // ********************************
+    // * INTERPOLATE
+    // ********************************
+
+    // Fill-in even-indexed rows
+    ulong start = 0;
+    for (ulong r = 0; r < rows.size(); r += 2) {
+        ulong row = rows[r];
+        ulong stop = _cols.size() – 1;
+        if (r < rows.size() – 1)
+            stop = col_idx_lookup[(*result)[rows[r + 1]]];
+        ulong argmin = _cols[start];
+        T min = lookup(row, argmin);
+        for (ulong c = start + 1; c <= stop; ++c) {
+            T value = lookup(row, _cols[c]);
+            if (c == start || value < min) {
+                argmin = _cols[c];
+                min = value;
+            }
+        }
+        (*result)[row] = argmin;
+        start = stop;
+    }
+}
+
+/*
+ *  Interface for the SMAWK algorithm, for finding the minimum value
+ *  in each row of an implicitly-defined totally monotone matrix.
+ */
+template <typename T>
+vector<ulong> smawk(
+        const ulong num_rows,
+        const ulong num_cols,
+        const function<T(ulong, ulong)>& lookup) {
+    vector<ulong> result;
+    result.resize(num_rows);
+    vector<ulong> rows(num_rows);
+    iota(begin(rows), end(rows), 0);
+    vector<ulong> cols(num_cols);
+    iota(begin(cols), end(cols), 0);
+    _smawk<T>(rows, cols, lookup, &result);
+    return result;
+}
+
+#define NUM_ROWS 9
+#define NUM_COLS 18
+
+// SMAWK works on implicitly defined matrices, utilizing a function
+// that returns a value as a function of matrix indices.
+// An explicitly defined matrix is used here for the purpose of
+// illustration.
+// The matrix is from:
+//   http://web.cs.unlv.edu/larmore/Courses/CSC477/monge.pdf.
+double matrix[NUM_ROWS][NUM_COLS] = {
+    { 25, 21, 13,10,20,13,19,35,37,41,58,66,82,99,124,133,156,178},
+    { 42, 35, 26,20,29,21,25,37,36,39,56,64,76,91,116,125,146,164},
+    { 57, 48, 35,28,33,24,28,40,37,37,54,61,72,83,107,113,131,146},
+    { 78, 65, 51,42,44,35,38,48,42,42,55,61,70,80,100,106,120,135},
+    { 90, 76, 58,48,49,39,42,48,39,35,47,51,56,63, 80, 86, 97,110},
+    {103, 85, 67,56,55,44,44,49,39,33,41,44,49,56, 71, 75, 84, 96},
+    {123,105, 86,75,73,59,57,62,51,44,50,52,55,59, 72, 74, 80, 92},
+    {142,123,100,86,82,65,61,62,50,43,47,45,46,46, 58, 59, 65, 73},
+    {151,130,104,88,80,59,52,49,37,29,29,24,23,20, 28, 25, 31, 39},
+};
+
+int main(void) {
+    auto lookup = [](ulong i,ulong j) {
+        return matrix[i][j];
+    };
+    vector<ulong> argmin = smawk<double>(NUM_ROWS, NUM_COLS, lookup);
+    for (ulong i = 0; i < argmin.size(); ++i) {
+        cout << argmin[i] << endl;
+    }
+    return 0;
+}
+
+=============
+//Hu-Shing for Matrix Chain Multiplication
+namespace HuShing {
+	using ll=long long;
+	using pii=pair<int,int>;
+	const int maxn=200010;
+	/* About Polygon */
+	int n;
+	ll w[maxn];
+	ll CP[maxn];
+	/* About H-arcs */
+	struct HArc {
+		int id; // calc in new_arc
+		int u,v; // "
+		int low; // "
+		ll base; // "
+		ll mul; // "
+		ll num,den; // calc in dfs
+		inline bool contains(HArc& b)const{
+			return u<=b.u&&b.v<=v;
+		}
+		inline ll get_support()const{
+			assert(den!=0);
+			return num/den;
+		}
+		bool operator<(const HArc& b)const{
+			return get_support()<b.get_support();
+		}
+		bool operator<=(const HArc& b)const{
+			return get_support()<=b.get_support();
+		}
+		bool operator==(const HArc& b)const{
+			return get_support()==b.get_support();
+		}
+	} h[maxn];
+
+	int n_arcs;
+	int sub[maxn]; // calc in dfs
+	vector<int> child[maxn]; // calc in build_tree
+	int n_pqs,qid[maxn]; // calc in dfs
+	priority_queue<HArc> pq[maxn];
+	vector<HArc> con[maxn];
+
+	void new_arc(int u,int v) {
+		assert(u<=v);
+		++n_arcs;
+		h[n_arcs].id=n_arcs;
+		h[n_arcs].u=u;
+		h[n_arcs].v=v;
+		h[n_arcs].low=w[u]<w[v]?u:v;
+		h[n_arcs].mul=(ll)w[u]*w[v];
+		h[n_arcs].base=CP[v]-CP[u]-h[n_arcs].mul;
+	}
+	void build_tree(vector<pii>& lst) {
+		vector<int> stk;
+		new_arc(1,n+1); // h[1] is root
+		for(auto& it:lst) {
+			new_arc(it.first,it.second);
+			while(!stk.empty()&&
+				h[n_arcs].contains(h[stk.back()])) {
+				child[n_arcs].push_back(stk.back());
+				stk.pop_back();
+			}
+			stk.push_back(n_arcs);
+		}
+		while(!stk.empty()) {
+			child[1].push_back(stk.back());
+			stk.pop_back();
+		}
+	}
+
+	void one_sweep() {
+		vector<int> stk;
+		vector<pii> tmp,lst;
+		for(int i=1;i<=n;i++) {
+			while(stk.size()>=2 && w[stk.back()]>w[i]) {
+				tmp.push_back({stk[stk.size()-2],i});
+				stk.pop_back();
+			}
+			stk.push_back(i);
+		}
+		while(stk.size()>=4) {
+			int Vt_1=stk[stk.size()-2];
+			tmp.push_back({1,Vt_1});
+			stk.pop_back();
+		}
+		for(auto& it:tmp) {
+			if(it.first==1||it.second==1) continue;
+			lst.push_back(it);
+		}
+		build_tree(lst);
+	}
+	void prepare() {
+		int V1=min_element(w+1,w+n+1)-w;
+		rotate(w+1,w+V1,w+n+1);
+		w[n+1]=w[1];
+		for(int i=1;i<=n+1;i++) {
+			CP[i]=(ll)w[i]*w[i-1];
+			CP[i]+=CP[i-1];
+		}
+	}
+	ll get_mn_mul(int node) {
+		if(node==1) return (ll)w[1]*w[2]+(ll)w[1]*w[n];
+		HArc& cur=h[node];
+		if(cur.u==cur.low) {
+			if(con[cur.u].empty()||!cur.contains(con[cur.u].back())) {
+				return (ll)w[cur.u]*w[cur.u+1];
+			} else return con[cur.u].back().mul;
+		} else {
+			if(con[cur.v].empty()||!cur.contains(con[cur.v].back())) {
+				return (ll)w[cur.v]*w[cur.v-1];
+			} else return con[cur.v].back().mul;
+		}
+		assert(0); // never happens
+		return 0;
+	}
+	inline void add_arc(int cur_node,HArc& arc) {
+		pq[qid[cur_node]].push(arc);
+		con[arc.u].push_back(arc);
+		con[arc.v].push_back(arc);
+	}
+	inline void remove_arc(int cur_node) {
+		const HArc& hm=pq[qid[cur_node]].top();
+		con[hm.u].pop_back();
+		con[hm.v].pop_back();
+		pq[qid[cur_node]].pop();
+	}
+	void merge_pq(int node) {
+		int max_child=-1;
+		for(auto& it:child[node])
+			if(max_child==-1||sub[max_child]<sub[it])
+				max_child=it;
+		qid[node]=qid[max_child];
+		auto& cur_pq=pq[qid[node]];
+		for(auto& it:child[node]) {
+			if(it==max_child) continue;
+			auto& child_pq=pq[qid[it]];
+			while(!child_pq.empty()) {
+				cur_pq.push(child_pq.top());
+				child_pq.pop();
+			}
+		}
+	}
+	void dfs(int node) {
+		HArc& cur=h[node];
+		sub[node]=1;
+		if(child[node].empty()) {
+			qid[node]=++n_pqs;
+			cur.den=cur.base;
+			cur.num=(ll)w[cur.low]*(cur.den+cur.mul-get_mn_mul(node));
+			add_arc(node,cur);
+			return;
+		}
+		cur.den=cur.base;
+		for(auto& it:child[node]) {
+			dfs(it);
+			sub[node]+=sub[it];
+			cur.den-=h[it].base;
+		}
+		cur.num=(ll)w[cur.low]*(cur.den+cur.mul-get_mn_mul(node));
+		merge_pq(node);
+		auto& cur_pq=pq[qid[node]];
+		while(!cur_pq.empty()&&cur_pq.top().get_support()>=w[cur.low]) {
+			auto hm=cur_pq.top();
+			cur.den+=hm.den;
+			remove_arc(node); // this must be done before calculating cur.num!
+			cur.num=(ll)w[cur.low]*(cur.den+cur.mul-get_mn_mul(node));
+		}
+		while(!cur_pq.empty()&&cur<=cur_pq.top()) {
+			auto hm=cur_pq.top();
+			cur.den+=hm.den;
+			remove_arc(node);
+			cur.num+=hm.num;
+		}
+		add_arc(node,cur);
+	}
+	ll getans() {
+		dfs(1);
+		ll ans=0;
+		auto& cur_pq=pq[qid[1]];
+		while(!cur_pq.empty()) {
+			ans+=cur_pq.top().num;
+			cur_pq.pop();
+		}
+		return ans;
+	}
+	void init() {
+		int i;
+		memset(w,0,sizeof(w));
+		memset(CP,0,sizeof(CP));
+		memset(sub,0,sizeof(sub));
+		memset(qid,0,sizeof(qid));
+		for(i=0;i<=n_pqs;i++) while(!pq[i].empty()) pq[i].pop();
+		for(i=0;i<=n;i++) { 
+			while(!con[i].empty()) con[i].pop_back();
+			while(!child[i].empty()) child[i].pop_back();
+		}
+		n_arcs=0;
+	}
+	void input(const vector<ll>& arr) {
+		n=arr.size();
+		for(int i=1;i<=n;i++) w[i]=arr[i-1];
+	}
+	ll solve(const vector<ll>& arr) {
+		if(arr.size()<2) return 0;
+		if(arr.size()==2) return arr[0]*arr[1];
+		init();
+		input(arr);
+		prepare();
+		one_sweep();
+		return getans();
+	}
+}
+
+int main() {
+	// three matrices: 5x3,3x2,2x6
+	printf("%lld\n",HuShing::solve({5,3,2,6}));
+	return 0;
+}
